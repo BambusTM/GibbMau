40ecaafb4150e8dbf245c896152f123ecb5b07fa
Bjørn Siekmann <yoru@Bjrns-MacBook-Air-2.local>
2023-09-11 08:41:50 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+  <div class="header">
+    <button class="button button1">Gib mau</button>
+    <button class="button button2">Home</button>
+  </div>
+
+<main>
+  <div>
+    <h1>Gibb mau - home</h1>
+  </div>
+  <div class="fileSelection">
+
+  </div>
+</main>
+
+<div class="footer">
+  <h2>Footer/Platzhalter</h2>
+  <p class="cp">copyright of your mom copyright of your mom copyright of your mom</p>
+</div>
