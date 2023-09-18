96d7c85a30a2279c1cf50c109249e983ae069d45
Bambus <sbjorb@gmail.com>
2023-09-18 10:44:03 +0200

    jo jetzt isch es halt nüm svelte

--- src/lib/Footer/Footer.svelte
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="src/lib/Footer.css">
-
-<div class="footer">
-  <h2>Footer/Platzhalter</h2>
-  <p class="cp">copyright of your mom copyright of your mom copyright of your mom</p>
-</div>
