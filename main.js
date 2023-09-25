6e88adf32e88d3bf523526402938cd4a5c82df0c
Bambus <sbjorb@gmail.com>
2023-09-25 20:25:35 +0200

    OMG ITS JUST ONE FUNCTION

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function openPopup() {
-    var popup1 = document.getElementById("myPopup");
-    popup1.classList.toggle("show");
-  } 
+function togglePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    if (popup) {
+      var allPopups = document.querySelectorAll(".popup");
+      allPopups.forEach(function (otherPopup) {
+        if (otherPopup !== popup) {
+          otherPopup.classList.remove("show");
+        }
+      });
+  
+      popup.classList.toggle("show");
+    }
+  }
+  
+function handleSettings(option) {
+    switch (option) {
+        case "option1":
+            break;
+        case "option2":
+            break;
+        default:
+            console.log("Ungültige Option");
+    }
+}
